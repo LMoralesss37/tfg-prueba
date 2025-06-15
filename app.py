6423d121f8cec402711d9721f0d27a740c341fc2
LMoralesss37 <tfg.lauramoralesfernandez@gmail.com>
2025-06-15 09:56:13 +0200

    Adaptado app.py para Render

---
+++ app.py
@@ -0,0 +1,55 @@
+import gradio as gr
+import pandas as pd
+import os
+
+# Función para procesar el CSV y aplicar filtros
+def procesar_csv(csv_file, nombre_filtro, apellidos_filtro, fecha_inicio_filtro, fecha_fin_filtro):
+    try:
+        df = pd.read_csv(csv_file.name)
+    except Exception:
+        df = pd.read_csv(csv_file.name, sep=None, engine='python')
+
+    df_limpio = df.copy()
+
+    df_limpio = df_limpio.dropna()
+    df_limpio.columns = df_limpio.columns.str.replace(';;', '', regex=False)
+    df_limpio['Hora de conexión'] = df_limpio['Hora de conexión'].str.split('.').str[0]
+    df_limpio['Tiempo de juego'] = df_limpio['Tiempo de juego'].str.split('.').str[0]
+    df_limpio['Fecha de conexión'] = pd.to_datetime(df_limpio['Fecha de conexión']).dt.strftime('%d/%m/%Y')
+    df_limpio['% tarea completado'] = df_limpio['% tarea completado'].apply(lambda x: f"{x}%")
+    df_limpio['Dolor'] = df_limpio['Dolor'].str.replace(';;', '', regex=False)
+
+    if nombre_filtro:
+        df_limpio = df_limpio[df_limpio['Nombre y apellidos'].str.contains(nombre_filtro, case=False, na=False)]
+    
+    if apellidos_filtro:
+        df_limpio = df_limpio[df_limpio['Nombre y apellidos'].str.contains(apellidos_filtro, case=False, na=False)]
+    
+    if fecha_inicio_filtro and fecha_fin_filtro:
+        df_limpio['Fecha de conexión'] = pd.to_datetime(df_limpio['Fecha de conexión'], format='%d/%m/%Y')
+        fecha_inicio = pd.to_datetime(fecha_inicio_filtro, format='%d/%m/%Y')
+        fecha_fin = pd.to_datetime(fecha_fin_filtro, format='%d/%m/%Y')
+        df_limpio = df_limpio[(df_limpio['Fecha de conexión'] >= fecha_inicio) & (df_limpio['Fecha de conexión'] <= fecha_fin)]
+
+    return df_limpio
+
+# Crear la interfaz de Gradio
+inputs = [
+    gr.File(label="Sube el archivo CSV"),
+    gr.Textbox(label="Filtro por Nombre"),
+    gr.Textbox(label="Filtro por Apellidos"),
+    gr.Textbox(label="Fecha de inicio (dd/mm/yyyy)", placeholder="Formato: dd/mm/yyyy"),
+    gr.Textbox(label="Fecha de fin (dd/mm/yyyy)", placeholder="Formato: dd/mm/yyyy")
+]
+
+outputs = gr.Dataframe(label="Datos filtrados")
+
+iface = gr.Interface(
+    fn=procesar_csv,
+    inputs=inputs,
+    outputs=outputs,
+    live=True
+)
+
+port = int(os.environ.get("PORT", 7860))
+iface.launch(server_name="0.0.0.0", server_port=port)
